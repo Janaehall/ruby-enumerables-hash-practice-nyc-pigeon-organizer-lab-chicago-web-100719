4c7d8d4d6faed020d0f83f21dbd097391aecae2a
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-27 02:09:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.values.each do |key|
     data.values.each do |val|
-
-    new_hash[val] = {} unless new_hash[val]
+      new_hash[val] = {} unless new_hash[val]
+    end
   end
   new_hash
 end
